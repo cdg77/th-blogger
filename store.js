36b0d3ac746758e918cbb2622e8c482680ae146c
cdg77 <cdegregorio@gmail.com>
2015-01-04 19:38:34 -0800

    added file of sample posts and stuff

---
+++ store.js
@@ -0,0 +1,41 @@
+var posts = [
+  {
+    id: '1',
+    title: 'A Post',
+    body: 'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex. Two driven jocks help fax my big quiz. Quick, Baz, get my woven flax jodhpurs! "Now fax quiz Jack!" my brave'
+  },
+  {
+    id: '2',
+    title: 'Another Post',
+    body: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally... -- CICERO'
+  },
+  {
+    id: '3',
+    title: 'And Yet Another Post',
+    body: 'There are many pleasant fictions of the law in constant operation, but there is not one so pleasant or practically humorous as that which supposes every man to be of equal value in its impartial eye, and the benefits of all laws to be equally attainable by all men, without the smallest reference to the furniture of their pockets. -- CHARLES DICKENS, Nicholas Nickleby'
+  }
+
+];
+
+var comments = [
+  {
+    id: '1',
+    postId: '3',
+    text: 'Great movie!'
+  },
+  {
+    id: '2',
+    postId: '2',
+    text: 'Who the heck is Cicero?!'
+  },
+  {
+    id: '3',
+    postId: '1',
+    text: 'huh?!'
+  },
+  {
+    id: '4',
+    postId: '3',
+    text: 'It might be a book too...?'
+  }
+];
