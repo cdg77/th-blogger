ccd02307afd9f973ff1d7304ecc58a353540e345
cdg77 <cdegregorio@gmail.com>
2015-01-05 20:20:05 -0800

    added routes

---
+++ routes/posts.js
@@ -0,0 +1,10 @@
+Blogger.PostsRoute = Ember.Route.extend ({
+  // This is how to overide controller and template if you want something other thatn the default shown here(lines:3-5)
+  // controllerName: 'posts',
+  // renderTemplate: function() {
+  //   this.render('posts');
+  // },
+  model: function() {
+    return posts;
+  }
+});
